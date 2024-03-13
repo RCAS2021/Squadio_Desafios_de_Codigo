893fee346747d54078b22c8bfe7d37aaa7259b41
Rafael Cruz <rcas2009@outlook.com>
2024-03-13 20:05:15 -0300

    Estrutura de Dados - Organizando Os Seus Ativos - Completo

---
+++ Desafios de Código SQUADIO - Intermediário I/Estrutura de Dados - Organizando Os Seus Ativos.py
@@ -0,0 +1,31 @@
+"""
+Descrição
+Após uma análise cuidadosa realizada pela equipe de desenvolvimento de uma empresa bancaria,
+foi identificado a necessidade de uma nova funcionalidade para otimizar os processos e melhorias da experiência dos usuários.
+Agora, sua tarefa é implementar uma solução que organize em ordem alfabética uma lista de ativos que será informada pelos usuários.
+Os ativos são representados por strings que representam seus tipos, como por exemplo: Reservas de liquidez, Ativos intangiveis e dentre outros.
+
+Entrada
+A primeira entrada consiste em um número inteiro que representa a  quantidade de ativos que o usuário possui.
+Em seguida, o usuário deverá  informar, em linhas separadas, os tipos (strings) dos respectivos ativos.
+
+Saída
+Seu programa deve exibir a lista de Ativos organizada em ordem alfabética. Cada ativo deve ser apresentado em uma linha separada.
+"""
+
+ativos = []
+
+# Entrada da quantidade de ativos
+quantidadeAtivos = int(input())
+
+# Entrada dos códigos dos ativos
+for _ in range(quantidadeAtivos):
+    codigoAtivo = input()
+    ativos.append(codigoAtivo)
+
+#TODO: Ordenar os ativos em ordem alfabética.
+ativos.sort()
+
+#TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+for i in range(quantidadeAtivos):
+  print(ativos[i] + "\n")
